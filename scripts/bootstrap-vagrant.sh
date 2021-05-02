c18bd8d9f007d069d584be8df1c67057bff52dad
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-05-02 08:04:52 +0800

    https://github.com/hashicorp/vagrant/releases/tag/v2.2.16

--- scripts/bootstrap-vagrant.sh
+++ scripts/bootstrap-vagrant.sh
@@ -22,27 +22,27 @@ cd "$(cd "$(dirname "$0")"; pwd -P)/../"
 if [ -x "$(command -v apt-get)" ]; then
     apt-get update
     apt-get install -y binutils bridge-utils dnsmasq-base ebtables gcc libguestfs-tools libvirt-clients libvirt-daemon-system libvirt-dev make qemu-system qemu-utils ruby-dev
-    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.15/vagrant_2.2.15_x86_64.deb
-    dpkg -i vagrant_2.2.15_x86_64.deb
-    rm -rf vagrant_2.2.15_*
+    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.16/vagrant_2.2.16_x86_64.deb
+    dpkg -i vagrant_2.2.16_x86_64.deb
+    rm -rf vagrant_2.2.16_*
 fi
 
 # Prepare YUM dependencies
 if [ -x "$(command -v yum)" ]; then
     yum makecache
     yum install -y binutils dnsmasq ebtables gcc libguestfs-tools-c libvirt libvirt-client libvirt-devel make qemu-img qemu-kvm ruby-devel virt-install
-    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.15/vagrant_2.2.15_x86_64.rpm
-    yum localinstall vagrant_2.2.15_x86_64.rpm
-    rm -rf vagrant_2.2.15_*
+    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.16/vagrant_2.2.16_x86_64.rpm
+    yum localinstall vagrant_2.2.16_x86_64.rpm
+    rm -rf vagrant_2.2.16_*
 fi
 
 # Prepare Zypper dependencies
 if [ -x "$(command -v zypper)" ]; then
     zypper -n --gpg-auto-import-keys refresh
     zypper -n install -y binutils-gold bridge-utils dnsmasq ebtables gcc guestfs-tools libvirt libvirt-client libvirt-devel make qemu-kvm qemu-tools ruby-devel virt-install
-    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.15/vagrant_2.2.15_x86_64.rpm
-    zypper install vagrant_2.2.15_x86_64.rpm
-    rm -rf vagrant_2.2.15_*
+    curl -skL -O https://releases.hashicorp.com/vagrant/2.2.16/vagrant_2.2.16_x86_64.rpm
+    zypper install vagrant_2.2.16_x86_64.rpm
+    rm -rf vagrant_2.2.16_*
 fi
 
 # Install vagrant-libvirt
